6015f927f713a914f901c71a1fdd0cb4d19049bf
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-07-30 11:52:04 +0530

    Updates

---
+++ precache-manifest.54879ed500d9d9f32a75ad1fb625bb54.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21b46091241b26f2be91178f9d69549",
+    "url": "/Rishie96/songs-car/index.html"
+  },
+  {
+    "revision": "fb716da3c7a02ec4ef8a",
+    "url": "/Rishie96/songs-car/static/css/main.4d369b83.chunk.css"
+  },
+  {
+    "revision": "6fc3b02c8a1aa2ed291b",
+    "url": "/Rishie96/songs-car/static/js/2.4827319d.chunk.js"
+  },
+  {
+    "revision": "fb716da3c7a02ec4ef8a",
+    "url": "/Rishie96/songs-car/static/js/main.2dc15dbd.chunk.js"
+  },
+  {
+    "revision": "7f39a3cc180de6e3a76b",
+    "url": "/Rishie96/songs-car/static/js/runtime~main.77fa7a07.js"
+  },
+  {
+    "revision": "84200631194a55c5d9b69636329f8bf9",
+    "url": "/Rishie96/songs-car/static/media/always.84200631.jpg"
+  },
+  {
+    "revision": "5ac53ad0a970da5d0fd1cbf0d52652c3",
+    "url": "/Rishie96/songs-car/static/media/borntodie.5ac53ad0.jpg"
+  },
+  {
+    "revision": "66448d1ef99e4d2f6f304f685e0ad393",
+    "url": "/Rishie96/songs-car/static/media/burn.66448d1e.jpg"
+  },
+  {
+    "revision": "124d7783163979aae5440242eeb9ce4c",
+    "url": "/Rishie96/songs-car/static/media/closer.124d7783.jpg"
+  },
+  {
+    "revision": "6bc582bb959ca354f443005a17544bbd",
+    "url": "/Rishie96/songs-car/static/media/dusktilldawn.6bc582bb.jpg"
+  },
+  {
+    "revision": "b92fb6c6b92ec683479f2477d3f6b17c",
+    "url": "/Rishie96/songs-car/static/media/electriclove.b92fb6c6.jpg"
+  },
+  {
+    "revision": "17be233defa79f093cb35b7cce023cb9",
+    "url": "/Rishie96/songs-car/static/media/faded.17be233d.jpg"
+  },
+  {
+    "revision": "3563e092c0b3668a227deb23816086c2",
+    "url": "/Rishie96/songs-car/static/media/itaintme.3563e092.jpg"
+  },
+  {
+    "revision": "f1abae04f6d1200025543184eaca5883",
+    "url": "/Rishie96/songs-car/static/media/paradisecity.f1abae04.jpg"
+  },
+  {
+    "revision": "db483dcecfb46e28f7229c91bc1ac56d",
+    "url": "/Rishie96/songs-car/static/media/style.db483dce.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Rishie96/songs-car/precache-manifest.54879ed500d9d9f32a75ad1fb625bb54.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rishie96/songs-car/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
